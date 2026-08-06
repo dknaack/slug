305515e75683c6a6d01ceecd22e65fd029d0c8bb
Daniel Knaack <daniel.knaack@protonmail.com>
2026-08-06 10:30:53 +0200

    Read the font file

--- main.c
+++ main.c
@@ -1,21 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <GLFW/glfw3.h>
+
+typedef struct {
+	char *at;
+	size_t length;
+} str;
+
+static str read_file(char *path)
+{
+	str result = {0};
+
+	FILE *file = fopen(path, "rb");
+	if (file) {
+		fseek(file, 0, SEEK_END);
+		result.length = ftell(file);
+		fseek(file, 0, SEEK_SET);
+
+		result.at = malloc(result.length);
+		fread(result.at, 1, result.length, file);
+	}
+
+	return result;
+}
 
 int main(void)
 {
-    GLFWwindow* window;
+	str file = read_file("fonts/OpenSans-Regular.ttf");
+	if (!file.at) {
+		return -1;
+	}
 
     if (!glfwInit()) {
 		return -1;
 	}
 
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window) {
         glfwTerminate();
         return -1;
     }
 
     glfwMakeContextCurrent(window);
-
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT);
 
