c52df9a61ce0b5c2790331291b8ca7ef3610f88d
Daniel Knaack <daniel.knaack@protonmail.com>
2026-08-17 14:10:31 +0200

    Draw a quad

--- main.c
+++ main.c
@@ -59,13 +59,62 @@ int main(void)
 
 	printf("Loaded OpenGL %d.%d\n", GLAD_VERSION_MAJOR(version), GLAD_VERSION_MINOR(version));
 
-    while (!glfwWindowShouldClose(window)) {
-        glClear(GL_COLOR_BUFFER_BIT);
+	const char *vertex_shader_source =
+		"#version 410 core\n"
+		"void main()\n"
+		"{\n"
+		"    const vec4 vertices[4] = vec4[4](\n"
+		"        vec4(-1.0, -1.0, 0.0, 1.0),\n"
+		"        vec4(+1.0, -1.0, 0.0, 1.0),\n"
+		"        vec4(-1.0, +1.0, 0.0, 1.0),\n"
+		"        vec4(+1.0, +1.0, 0.0, 1.0)\n"
+		"    );\n"
+		"\n"
+		"    gl_Position = vertices[gl_VertexID];\n"
+		"}\n";
+	GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertex_shader, 1, &vertex_shader_source, NULL);
+	glCompileShader(vertex_shader);
 
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
+	const char *fragment_shader_source =
+		"#version 410 core\n"
+		"out vec4 frag_color;\n"
+		"void main()\n"
+		"{\n"
+		"	frag_color = vec4(1.0);\n"
+		"}\n";
+	GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragment_shader, 1, &fragment_shader_source, NULL);
+	glCompileShader(fragment_shader);
 
-    glfwTerminate();
-    return 0;
+	GLuint program = glCreateProgram();
+	glAttachShader(program, vertex_shader);
+	glAttachShader(program, fragment_shader);
+	glLinkProgram(program);
+
+	int success = 0;
+	glGetProgramiv(program, GL_LINK_STATUS, &success);
+	if (!success) {
+		char info_log[1024] = {0};
+		glGetProgramInfoLog(program, sizeof(info_log) - 1, NULL, info_log);
+		printf("Failed to compile shader: %s\n", info_log);
+		return -1;
+	}
+
+	glUseProgram(program);
+
+	GLuint vertex_array = 0;
+	glGenVertexArrays(1, &vertex_array);
+	glBindVertexArray(vertex_array);
+
+	while (!glfwWindowShouldClose(window)) {
+		glClear(GL_COLOR_BUFFER_BIT);
+		glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
 }
