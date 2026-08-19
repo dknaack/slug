8020d34ebb9c736f4eec1faa0e1355919a743173
Daniel Knaack <daniel.knaack@protonmail.com>
2026-08-19 13:25:51 +0200

    Implement slug algorithm

--- frag.glsl
+++ frag.glsl
@@ -7,79 +7,14 @@ uniform isampler1D contour_data;
 
 out vec4 frag_color;
 
-int intersects(vec2 p, vec2 p0, vec2 p1, vec2 p2)
-{
-    float a = p0.y - 2.0*p1.y + p2.y;
-    float b = 2.0*(p1.y - p0.y);
-    float c = p0.y - p.y;
-
-    // Linear case
-    if (abs(a) < 1e-6)
-    {
-        if (abs(b) < 1e-6)
-            return 0;
-
-        float t = -c / b;
-
-        if (t < 0.0 || t > 1.0)
-            return 0;
-
-        float x = (1.0-t)*(1.0-t)*p0.x
-                + 2.0*(1.0-t)*t*p1.x
-                + t*t*p2.x;
-
-        if (x < p.x)
-            return 0;
-
-        return b > 0.0 ? 1 : -1;
-    }
-
-    float d = b*b - 4.0*a*c;
-
-    if (d < 0.0)
-        return 0;
-
-    float s = sqrt(d);
-    float t0 = (-b - s) / (2.0*a);
-    float t1 = (-b + s) / (2.0*a);
-
-    // Test both intersections.
-    if (t0 >= 0.0 && t0 <= 1.0)
-    {
-        float x = (1.0-t0)*(1.0-t0)*p0.x
-                + 2.0*(1.0-t0)*t0*p1.x
-                + t0*t0*p2.x;
-
-        if (x >= p.x)
-        {
-            float dy = b + 2.0*a*t0;
-
-            if (abs(dy) > 1e-6)
-                return dy > 0.0 ? 1 : -1;
-        }
-    }
-
-    if (t1 >= 0.0 && t1 <= 1.0)
-    {
-        float x = (1.0-t1)*(1.0-t1)*p0.x
-                + 2.0*(1.0-t1)*t1*p1.x
-                + t1*t1*p2.x;
-
-        if (x >= p.x)
-        {
-            float dy = b + 2.0*a*t1;
-
-            if (abs(dy) > 1e-6)
-                return dy > 0.0 ? 1 : -1;
-        }
-    }
-
-    return 0;
-}
-
 void main()
 {
+	const float epsilon = 0.0001;
+
+	float coverage = 0.0;
 	frag_color = vec4(1.0);
+
+	vec2 pixels_per_em = vec2(1.0 / fwidth(pos.x), 1.0 / fwidth(pos.y));
 
 	int num_crossings = 0;
 	int contour_count = 2;
@@ -92,14 +27,47 @@ void main()
 			vec2 p1 = texelFetch(point_data, contour_start + (j + 1) % point_count, 0).rg;
 			vec2 p2 = texelFetch(point_data, contour_start + (j + 2) % point_count, 0).rg;
 
-			num_crossings += intersects(pos, p0, p1, p2);
+			p0 -= pos;
+			p1 -= pos;
+			p2 -= pos;
+
+			uint shift = ((p0.y > 0.0) ? 2U : 0U) + ((p1.y > 0.0) ? 4U : 0U) + ((p2.y > 0.0) ? 8U : 0U);
+			uint code = (0x2E74U >> shift) & 3U;
+			if (code != 0U) {
+				float ay = p0.y - p1.y * 2.0 + p2.y;
+				float by = p0.y - p1.y;
+				float cy = p0.y;
+
+				float t1, t2;
+				if (abs(ay) < epsilon) {
+					t1 = t2 = p0.y / (2 * by);
+				} else {
+					float d = sqrt(max(by * by - ay * cy, 0.0));
+					t1 = (by - d) / ay;
+					t2 = (by + d) / ay;
+				}
+
+				float ax = p0.x - p1.x * 2.0 + p2.x;
+				float bx = p0.x - p1.x;
+
+				if ((code & 1U) != 0U) {
+					float x1 = (ax * t1 - bx * 2.0) * t1 + p0.x;
+					coverage += clamp(x1 * pixels_per_em.x + 0.5, 0.0, 1.0);
+				}
+
+				if ((code & 2U) != 0) {
+					float x2 = (ax * t2 - bx * 2.0) * t2 + p0.x;
+					coverage -= clamp(x2 * pixels_per_em.x + 0.5, 0.0, 1.0);
+				}
+			}
 		}
 
 		contour_start = contour_end;
 	}
 
-	if (num_crossings % 2 != 0) {
-		frag_color.rgb = vec3(0);
-	}
+	vec4 vcolor = vec4(0.0, 1.0, 1.0, 1.0);
+	coverage = sqrt(clamp(abs(coverage) * 0.5, 0.0, 1.0));
+	float alpha = coverage * vcolor.w;
+	frag_color = vec4(vcolor.xyz * alpha, alpha);
 }
 
