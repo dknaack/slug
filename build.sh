23bb9e236ddec43369255cc48972bbc3e2113e86
Daniel Knaack <daniel.knaack@protonmail.com>
2026-09-20 13:33:52 +0200

    Add library flags to build script

--- build.sh
+++ build.sh
@@ -2,9 +2,12 @@
 
 case "$(uname)" in
 	Darwin)
-		cc -g3 -Wall -o main main.c `pkg-config --cflags --libs glfw3` -framework OpenGL
+		cc -g3 -Wall -Wno-unused \
+			-fsanitize-trap -fsanitize-undefined-trap-on-error -fsanitize=undefined,address \
+			-o main main.c `pkg-config --cflags --libs glfw3` \
+			-framework OpenGL -framework CoreText -framework CoreFoundation
 		;;
 	*)
-		cc -g3 -Wall -o main main.c `pkg-config --cflags --libs glfw3` -lGL
+		cc -g3 -Wall -Wno-unused -o main main.c `pkg-config --cflags --libs glfw3` -lGL
 		;;
 esac
